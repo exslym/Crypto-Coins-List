93aac20c1942332c3049231dd6c3ce96dbcefb65
exslym <76844562+exslym@users.noreply.github.com>
2023-04-19 17:45:30 +0400

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
 import 'package:crypto_coins_list/crypto_coins_list_app.dart';
+import 'package:crypto_coins_list/firebase_options.dart';
 import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
+
 import 'package:dio/dio.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -13,14 +16,19 @@ import 'package:talker_flutter/talker_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   final talker = TalkerFlutter.init();
   GetIt.I.registerSingleton(talker);
   GetIt.I<Talker>().debug('Talker started...');
 
+  final app = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  talker.info(app.options.projectId);
+
   const cryptoCoinsBoxName = 'crypto_coins_box';
 
   await Hive.initFlutter();
-
   Hive.registerAdapter(CryptoCoinAdapter());
   Hive.registerAdapter(CryptoCoinDetailAdapter());
 
