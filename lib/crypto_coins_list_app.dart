4e00e8b6932178dc38b4feb033f96ac36d208d23
exslym <76844562+exslym@users.noreply.github.com>
2023-04-20 15:14:12 +0400

    AutoRoute added, screens updated

--- lib/crypto_coins_list_app.dart
+++ lib/crypto_coins_list_app.dart
@@ -1,24 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:crypto_coins_list/router/router.dart';
-import 'package:crypto_coins_list/theme/theme.dart';
 import 'package:get_it/get_it.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
-class CryptoCurrenciesListApp extends StatelessWidget {
+import 'package:crypto_coins_list/router/router.dart';
+import 'package:crypto_coins_list/theme/theme.dart';
+
+class CryptoCurrenciesListApp extends StatefulWidget {
   const CryptoCurrenciesListApp({super.key});
 
   @override
+  State<CryptoCurrenciesListApp> createState() =>
+      _CryptoCurrenciesListAppState();
+}
+
+class _CryptoCurrenciesListAppState extends State<CryptoCurrenciesListApp> {
+  final _appRouter = AppRouter();
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Cryptocurrency Prices',
       theme: darkTheme,
-      routes: routes,
-      navigatorObservers: [
-        TalkerRouteObserver(
-          GetIt.I<Talker>(),
-        )
-      ],
+      routerConfig: _appRouter.config(
+        navigatorObservers: () => [
+          TalkerRouteObserver(
+            GetIt.I<Talker>(),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/features/crypto_list/widgets/crypto_coin_tile.dart
+++ lib/features/crypto_list/widgets/crypto_coin_tile.dart
@@ -1,4 +1,6 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
+import 'package:crypto_coins_list/router/router.dart';
 import 'package:flutter/material.dart';
 
 class CryotoCoinTile extends StatelessWidget {
@@ -25,10 +27,11 @@ class CryotoCoinTile extends StatelessWidget {
       ),
       trailing: const Icon(Icons.arrow_forward_ios),
       onTap: () {
-        Navigator.of(context).pushNamed(
-          '/coin',
-          arguments: coin,
-        );
+        AutoRouter.of(context).push(CryptoCoinRoute(coin: coin));
+        // Navigator.of(context).pushNamed(
+        //   '/coin',
+        //   arguments: coin,
+        // );
       },
     );
   }
